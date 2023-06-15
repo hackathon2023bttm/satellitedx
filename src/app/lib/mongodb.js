0a8cd8b19b6c1ad59205c252873463f076f67472
Matthew Tso <matthewt@wepay.com>
2023-06-14 17:37:08 -0700

    Un-require env var in dev

--- src/app/lib/mongodb.js
+++ src/app/lib/mongodb.js
@@ -1,38 +1,12 @@
-// const mongoose = require("mongoose");
-
-// const uri = process.env.MONGODB_URI
-// const options = {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// };
-
-// if (!process.env.MONGODB_URI) {
-//   throw new Error('Please add your Mongo URI to .env.local')
-// }
-
-// let client
-
-// // if (process.env.NODE_ENV === 'development') {
-// //   console.log('hasmongoclientprom', !!global._mongoClientPromise)
-// //   if (!global._mongoClientPromise) {
-// //     client = mongoose.connect(uri, options);
-// //     global._mongoClientPromise = client.getClient();
-// //   }
-// // } else {
-// //   client = mongoose.connect(uri, options);
-// // }
-
-// client = mongoose.connect(uri, options);
-// module.exports = client;
-
-
 import mongoose from 'mongoose'
 
 const MONGODB_URI = process.env.MONGODB_URI
 
-if (!MONGODB_URI) {
+const isDev = process.env.NODE_ENV === 'development';
+
+if (!isDev && !MONGODB_URI) {
   throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
+    'Please define the MONGODB_URI environment variable'
   )
 }
 
